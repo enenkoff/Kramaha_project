f609ce44f7f66036d70cddf065e74a783d807385
vipflame <djflame1986@gmail.com>
2019-02-15 20:40:29 +0200

    test commit

--- tasks/Task001/script.js
+++ tasks/Task001/script.js
@@ -3,7 +3,7 @@
 function firstDuplicate(a) {
     var num;
 
-    //  код
+    /*test script 1*/
 
     return num;
 }
@@ -11,3 +11,4 @@ function firstDuplicate(a) {
 let data = [2, 1, 3, 5, 3, 2];
 
 document.getElementById('app').innerHTML = firstDuplicate(data);
+
