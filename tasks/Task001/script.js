cbc2441309ce9d41c7c19b1d4caaa6d2bf514c69
vipflame <djflame1986@gmail.com>
2019-02-17 14:42:58 +0200

    task001

--- tasks/Task001/script.js
+++ tasks/Task001/script.js
@@ -1,14 +1,21 @@
 'use strict'
 
-function firstDuplicate(a) {
-    var num;
+function firstDuplicate(arr) {
 
-    /*test script 2*/
+    var arr2 = [];
+    for (var j = 0; j < arr.length; j++) {
+        var num = arr[j];
 
-    return num;
+        for (var i = j + 1; i < arr.length; i++) {
+            if (num == arr[i]) arr2.push(i);
+        }
+    }
+    if (arr2.length > 0) return arr[Math.min.apply(null, arr2)];
+    return -1;
+
 }
 
-let data = [2, 1, 3, 5, 3, 2];
+let data = [1, 2, 3, 4, 1];
 
 document.getElementById('app').innerHTML = firstDuplicate(data);
 
