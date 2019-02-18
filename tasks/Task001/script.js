676ff66a22bce9b0472632edc2d842de7c48353c
Home@DESKTOP-C9PVQTT.(none) <Home@DESKTOP-C9PVQTT.(none)>
2019-02-18 21:33:43 +0200

    выполнено

--- tasks/Task001/script.js
+++ tasks/Task001/script.js
@@ -3,11 +3,20 @@
 function firstDuplicate(a) {
     var num;
 
-    //  код
-
+    for(var i = 0; i < a.length; i++){
+        for(var j = i + 1; j < a.length; j++){
+            if(a[i] == a[j]){
+                num = a[i];
+                break;
+            }
+        }
+    }
+    if(num == undefined){
+        num = -1;
+    }
     return num;
 }
 
-let data = [2, 1, 3, 5, 3, 2];
+let data = [499];
 
-document.getElementById('app').innerHTML = firstDuplicate(data);
+document.getElementById('app').innerHTML = firstgitDuplicate(data);
--- tasks/Task003/script.js
+++ tasks/Task003/script.js
@@ -1,13 +1,39 @@
 'use strict'
 
 function isValidWalk(walk) {
+    if(walk.length<1 || walk.length>20){
+        return -1;
+    }
     var timely;
-
-    //  код
+    var x = 0;
+    var y = 0;
+     for(var i in walk){
+         switch(walk[i]) {
+             case 'N':
+                x +=1;
+                break;
+             case 'E':
+                 y +=1;
+                 break;
+             case 'S':
+                 x -=1;
+                 break;
+             case 'W':
+                 y -=1;
+                 break;
+             default:
+                     return -1;
+         }
+     }
+     if(x==0 && y==0 && walk.length==10){
+         timely = true;
+     }else {
+         timely = false;
+     }
 
     return timely;
 }
 
-let data = [N, E, N, E, S, W, S, S, W, N];
+let data = ['N', 'E', 'N', 'E', 'S', 'W', 'S', 'S', 'W', 'N'];
 
 document.getElementById('app').innerHTML = isValidWalk(data);
