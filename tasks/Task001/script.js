67a6fe733c1a7af726ef631105d3c74fa7104c72
Home@DESKTOP-C9PVQTT.(none) <Home@DESKTOP-C9PVQTT.(none)>
2019-02-18 21:58:39 +0200

    выполнил

--- tasks/Task001/script.js
+++ tasks/Task001/script.js
@@ -2,21 +2,25 @@
 
 function firstDuplicate(a) {
     var num;
-
-    for(var i = 0; i < a.length; i++){
-        for(var j = i + 1; j < a.length; j++){
-            if(a[i] == a[j]){
-                num = a[i];
-                break;
+    if(a.length>=1 && a.length<=500){
+        for(var i = 0; i < a.length; i++){
+            for(var j = i + 1; j < a.length; j++){
+                if(a[i] == a[j]){
+                    num = a[i];
+                    break;
+                }
             }
         }
+        if(num == undefined){
+            num = -1;
+        }
+    }else {
+        return -1;
     }
-    if(num == undefined){
-        num = -1;
-    }
+
     return num;
 }
 
-let data = [499];
+let data = [2, 1, 3, 5, 3, 2];
 
-document.getElementById('app').innerHTML = firstgitDuplicate(data);
+document.getElementById('app').innerHTML = firstDuplicate(data);
--- tasks/Task002/script.js
+++ tasks/Task002/script.js
@@ -2,13 +2,34 @@
 
 function almostIncreasingSequence(sequence) {
     var isIncreasing;
+    var countDel=0;
 
-    //  код
+
+    if (sequence.length == 1){
+        return isIncreasing = true;
+    }
+    if (sequence.length>=2 && sequence.length<=Math.pow(10, 5)) {
+        for (var i=1; i<sequence.length; i++){
+
+            if(sequence[i-1]< sequence[i]){
+                isIncreasing = true;
+            } else if(countDel<1){
+                countDel = 1;
+                sequence.splice(i,1);
+                if(sequence[i-1]< sequence[i]){
+                    isIncreasing = true;
+                }else isIncreasing = false;
+            }
+
+            else isIncreasing = false;
+        }
+    } else return -1;
+
 
     return isIncreasing;
 }
 
 
-let data = [1, 3, 2, 1];
+let data =  [1, 3, 2, 5];
 
 document.getElementById('app').innerHTML = almostIncreasingSequence(data);
